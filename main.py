583ab0a7798cf50e58f017535440ea4b7503ffe1
Jam1-git <james.w.munday@gmail.com>
2025-08-01 00:26:49 +0100

    Asteroid game with movable player, asteroids, collisions and shooting. Shooting effect not implemented

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 from textwrap import fill
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+import sys
+from shot import Shot
+
 def main():
     pygame.init() 
+    
     clock = pygame.time.Clock() 
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+    my_player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    new_asteroid_field = AsteroidField()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -15,7 +32,14 @@ def main():
                 pygame.quit()
                 return
         screen.fill("black")
-        pygame.display.flip()
+        for drawable_object in drawable:
+            drawable_object.draw(screen)
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collision_check(my_player):
+                print("Game over!")
+                sys.exit()
+        pygame.display.flip()   
         dt = clock.tick(60)/1000  # Limit to 60 FPS
 
 if __name__ == "__main__":
